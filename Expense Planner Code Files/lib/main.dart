8feaa7f574bc4bb1b1c2e8dab69ff0726186c4e9
Aakanksh Singh <72185365+aakankshsingh02@users.noreply.github.com>
2022-07-02 00:19:36 +0530

    Add files via upload

--- Expense Planner Code Files/lib/main.dart
+++ Expense Planner Code Files/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,24 +20,104 @@ class MyApp extends StatelessWidget {
 }
 
 class MyWidget extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 560.80,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New Shirt',
+      amount: 480.9,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter App"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Card(
-              color: Colors.blue,
-              // ignore: sort_child_properties_last
-              child: Container(width: 100, child: const Text("Chart")),
-              elevation: 5,
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+                color: Colors.blue,
+                // ignore: sort_child_properties_last
+                child: Text("Chart"),
+                elevation: 5),
+          ),
+          Card(
+            elevation: 10,
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  FlatButton(
+                    textColor: Colors.orange,
+                    onPressed: () {},
+                    child: Text("Add Transaction"),
+                  ),
+                ],
+              ),
             ),
-            const Card(
-              child: Text("List of TX"),
-            )
-          ],
-        ));
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.black,
+                      width: 3,
+                    )),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromRGBO(244, 67, 54, 1)),
+                      "\$${tx.amount}",
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMMd().format(tx.date),
+                        style: const TextStyle(color: Colors.green),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
